33eb2c288b9c549ebc6a9d4cb6f72edfcd1009df
hutongfei <13507469929@163.com>
2021-04-21 08:26:58 +0800

    11

--- src/main/java/com/my/controller/DemoController.java
+++ src/main/java/com/my/controller/DemoController.java
@@ -1,16 +1,12 @@
 package com.my.controller;
 
-import com.my.annotation.LimitAnnotation;
-import com.my.entity.Pro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 
 /**
